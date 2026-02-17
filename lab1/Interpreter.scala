ea0ca585a3faaebc989e084870bcae8965bc0ad1
AgentNoon239 <tom@maldenmail.co.uk>
2026-02-17 14:35:16 +0000

    interpreter boilerplate

--- lab1/Interpreter.scala
+++ lab1/Interpreter.scala
@@ -12,12 +12,37 @@ import cdim.error._
 
 class Interpreter (val prog: Prog) {
 
-	private variables: List[HashMap] = new List()
-	private treeStack: Stack = new Stack()
+	private variables: new HashMap[String, Int] = new HashMap[String, Int]()
 
 	def execute = {
-
+		for (stmt <- prog.stmts) {
+			executeStmt(stmt)
+		}
 	}
+
+	private def executeStmt(stmt: Stmt) = {
+		stmt match {
+			AssignStmt(name, expr) => {
+				variables(name) = executeExpr(expr)
+			}
+			SeqStmt(stmts) => {
+				for (stmt <- stmts) {
+					executeStmt(stmt)
+				}
+			}
+			IfStmt(expr, thenStmt, elseStmt) => {
+				if (executeExpr(expr)) {
+					executeStmt(thenStmt)
+				} else {
+					executeStmt(elseStmt)
+				}
+			}
+			WhileStmt(expr, stmt) => {
+				while (executeExpr(expr)) {
+					executeStmt(stmt)
+				}
+			}
+		}
 
 	private def executeExpr(expr: Expr) = {
 		expr match {
@@ -33,6 +58,30 @@ class Interpreter (val prog: Prog) {
 			case BinaryExpr(expr1, DIV, expr2) => {
 				executeExpr(expr1) / executeExpr(expr2)
 			}
+			case BinaryExpr(exp1, EQ, expr2) => {
+				executeExpr(exp1) == executeExpr(expr2)
+			}
+			case BinaryExpr(exp1, NEQ, expr2) => {
+				executeExpr(exp1) != executeExpr(expr2)
+			}
+			case BinaryExpr(exp1, LT, expr2) => {
+				executeExpr(exp1) < executeExpr(expr2)
+			}
+			case BinaryExpr(exp1, GT, expr2) => {
+				executeExpr(exp1) > executeExpr(expr2)
+			}
+			case BinaryExpr(exp1, LTE, expr2) => {
+				executeExpr(exp1) <= executeExpr(expr2)
+			}
+			case BinaryExpr(exp1, GTE, expr2) => {
+				executeExpr(exp1) >= executeExpr(expr2)
+			}
+			case BinaryExpr(exp1, AND, expr2) => {
+				executeExpr(exp1) && executeExpr(expr2)
+			}
+			case BinaryExpr(exp1, OR, expr2) => {
+				executeExpr(exp1) || executeExpr(expr2)
+			}
 			case UnaryExpr(expr1, UMINUS) => {
 				-executeExpr(expr1)
 			}
@@ -40,19 +89,21 @@ class Interpreter (val prog: Prog) {
 				!executeExpr(expr1)
 			}
 			case Variable(name: String) => {
-
+				variables.get(name) match {
+					case Some(value) => value
+					case None => throw new RuntimeError("Variable " + name + " not defined")
+				}
+			}
+			case Num(value) => {
+				value
+			}
+			case TernaryExpr(exprcond, exprt, exprf) => {
+				if (executeExpr(exprcond)) {
+					executeExpr(exprt)
+				} else {
+					executeExpr(exprf)
+				}
 			}
 		}
 	}
-
-	private def resolveVar(name: String) = {
-		while (true) {
-			val h = variables.headOption
-			scope = 
-		}
-	}
-
-
-
-
 }
